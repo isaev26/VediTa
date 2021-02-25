f747d7ef0095b3bf6de1d617314ccf0f80bd0bdc
isaev26 <islam.isaev2606@gmail.com>
2021-02-25 22:55:27 +0300

    task2 template

---
+++ local/templates/.default/components/bitrix/catalog.section/task2table/template.php
@@ -0,0 +1,85 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="catalog-section">
+	<?
+echo "<pre>";
+// print_r($arResult["ITEMS"][0]["OFFERS"][0]["PRICES"]["BASE"]["VALUE"]);
+echo "</pre>";
+?>
+	<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+	<p><?= $arResult["NAV_STRING"] ?></p>
+	<?endif?>
+	<table class="data-table" cellspacing="0" cellpadding="0" border="0" width="100%">
+		<thead>
+			<tr>
+				<td><?= GetMessage("CATALOG_TITLE") ?></td>
+				<?if(count($arResult["ITEMS"]) > 0):
+			foreach($arResult["ITEMS"][0]["DISPLAY_PROPERTIES"] as $arProperty):?>
+				<td><?= $arProperty["NAME"] ?></td>
+				<?endforeach;
+		endif;?>
+				<?foreach($arResult["PRICES"] as $code=>$arPrice):?>
+				<td><?= $arPrice["TITLE"] ?></td>
+				<?endforeach?>
+				<?if(count($arResult["PRICES"]) > 0):?>
+				<td>&nbsp;</td>
+				<?endif?>
+			</tr>
+		</thead>
+		<?foreach($arResult["ITEMS"] as $arElement):?>
+		<?
+	$this->AddEditAction($arElement['ID'], $arElement['EDIT_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arElement['ID'], $arElement['DELETE_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BCS_ELEMENT_DELETE_CONFIRM')));
+	?>
+		<tr id="<?= $this->GetEditAreaId($arElement['ID']); ?>">
+			<td>
+				<a href="<?= $arElement["DETAIL_PAGE_URL"] ?>"><?= $arElement["NAME"] ?></a>
+				<?if(count($arElement["SECTION"]["PATH"])>0):?>
+				<br />
+				<?foreach($arElement["SECTION"]["PATH"] as $arPath):?>
+				/ <a href="<?= $arPath["SECTION_PAGE_URL"] ?>"><?= $arPath["NAME"] ?></a>
+				<?endforeach?>
+				<?endif?>
+			</td>
+			<?foreach($arElement["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+			<td>
+				<?if(is_array($arProperty["DISPLAY_VALUE"]))
+				echo implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);
+			elseif($arProperty["DISPLAY_VALUE"] === false)
+				echo "&nbsp;";
+			else
+				echo $arProperty["DISPLAY_VALUE"];?>
+			</td>
+			<?endforeach?>
+			<?foreach($arResult["PRICES"] as $code=>$arPrice):?>
+			<td>
+				<?if($arPrice = $arElement["PRICES"][$code]):?>
+				<?if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]):?>
+				<s><?= $arPrice["PRINT_VALUE"] ?></s><br /><span class="catalog-price"><?= $arPrice["PRINT_DISCOUNT_VALUE"] ?></span>
+				<?else:?>
+				<span class="catalog-price"><?= $arPrice["PRINT_VALUE"] ?></span>
+				<?endif?>
+				<?else:?>
+				<span class="catalog-price"><?= $arElement["OFFERS"][0]["PRICES"][$code]["VALUE"] ?></span>
+				<?endif;?>
+			</td>
+			<?endforeach;?>
+		</tr>
+		<?endforeach;?>
+	</table>
+	<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+	<p><?= $arResult["NAV_STRING"] ?></p>
+	<?endif?>
+</div>
